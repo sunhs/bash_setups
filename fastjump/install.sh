308a1131dacbf5dbe2fcbff2d7110cd204ee7162
zhuhaisheng <zhuhaisheng@bigo.sg>
2021-04-21 15:50:58 +0800

    fix

--- fastjump/install.sh
+++ fastjump/install.sh
@@ -3,4 +3,4 @@ echo "Installing fastjump."
 echo "========================================================="
 git clone git@github.com:sunhs/fastjump.git tmp_fastjump
 cd tmp_fastjump/main && make
-rm -rf tmp_fastjump
+cd ../.. && rm -rf tmp_fastjump
