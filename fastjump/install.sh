7b5ab0baa5d48583bda9cdc639c6b28c3cf02a2c
hyesun <hyesunzhu@outlook.com>
2021-07-16 16:06:27 +0800

    fix install pyfj

--- fastjump/install.sh
+++ fastjump/install.sh
@@ -2,6 +2,4 @@
 # cd tmp_fastjump/main && make
 # cd ../.. && rm -rf tmp_fastjump
 
-git clone git@github.com:sunhs/pyfj.git tmp_pyfj
-cd tmp_pyfj && pip install -e .
-cd .. && rm -rf tmp_pyfj
+pip install git+https://github.com/sunhs/pyfj.git
