4bd524116bf13598c1232fab8a316b0a0dd2348b
zhuhaisheng <zhuhaisheng@bigo.sg>
2021-07-16 15:58:05 +0800

    use pyfj

--- fastjump/install.sh
+++ fastjump/install.sh
@@ -1,3 +1,7 @@
-git clone git@github.com:sunhs/fastjump.git tmp_fastjump
-cd tmp_fastjump/main && make
-cd ../.. && rm -rf tmp_fastjump
+# git clone git@github.com:sunhs/fastjump.git tmp_fastjump
+# cd tmp_fastjump/main && make
+# cd ../.. && rm -rf tmp_fastjump
+
+git clone git@github.com:sunhs/pyfj.git tmp_pyfj
+cd tmp_pyfj && pip install -e .
+cd .. && rm -rf tmp_pyfj
