19a5d59931e18496fe537fbca769ed9d0ab192fc
zhuhaisheng <zhuhaisheng@bigo.sg>
2023-01-03 16:15:24 +0800

    add blank lines in log

--- setup.sh
+++ setup.sh
@@ -17,13 +17,13 @@ NOCOLOR="\e[0m"
 printf "${GREEN}>>>>>>>>>> Installing bash/zsh${NOCOLOR}\n"
 cd shrc && bash install.sh $shell_chosen $platform && cd - || exit 1
 
-printf "${GREEN}>>>>>>>>>> Installing fastjump${NOCOLOR}\n"
+printf "\n\n\n${GREEN}>>>>>>>>>> Installing fastjump${NOCOLOR}\n"
 cd fastjump && bash install.sh && cd - || exit 1
 
 #printf "${GREEN}>>>>>>>>>> Installing vim color scheme${NOCOLOR}\n"
 #cd vim && bash install.sh && cd - || exit 1
 
 if [ $platform = "linux" ]; then
-    printf "${GREEN}>>>>>>>>>> Installing tmux${NOCOLOR}\n"
+    printf "\n\n\n${GREEN}>>>>>>>>>> Installing tmux${NOCOLOR}\n"
     cd tmux && bash install.sh && cd - || exit 1
 fi
