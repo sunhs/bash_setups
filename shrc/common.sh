95fa7788f9d1b88ba2afd8adca84c2224536bc8c
hyesun <hyesunzhu@outlook.com>
2023-02-02 14:24:09 +0800

    mod LC_ALL

--- shrc/common.sh
+++ shrc/common.sh
@@ -3,7 +3,8 @@
 
 #################### common section ####################
 export LANG=en_US.UTF-8
-export LC_ALL=C.UTF-8
+# export LC_ALL=C.UTF-8
+export LC_ALL=en_US.UTF-8
 
 if [ -f $HOME/.terminfo/x/xterm-24bit ]; then
     export TERM=xterm-24bit
