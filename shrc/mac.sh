3c5f40432d0e61663253c2db3745b3ec9120c84a
hyesun <hyesunzhu@outlook.com>
2023-02-07 15:03:30 +0800

    add conda init to mac.sh

--- shrc/mac.sh
+++ shrc/mac.sh
@@ -15,3 +15,18 @@ alias sg="ssh -t sg 'TERM=screen-256color tmux a -t fuck -d'"
 alias cg="cargo"
 alias ls='ls -G'
 alias ll='ls -lG'
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/Users/hyesun/conda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/Users/hyesun/conda/etc/profile.d/conda.sh" ]; then
+        . "/Users/hyesun/conda/etc/profile.d/conda.sh"
+    else
+        export PATH="/Users/hyesun/conda/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
